46cd308eec3e4d9527541db065887c7fbd2b3425
kohonda <0905honda@gmail.com>
2021-06-09 09:18:22 +0900

    support win

--- client.cpp
+++ client.cpp
@@ -1,21 +1,22 @@
+#include <iostream>
 #include "udp_lib_switcher.hpp"
 #include "msg_A.hpp"
 
 int main()
 {
 
-    udp::UDPLib client("127.0.0.1", 4001);
+	udp::UDPLib client("127.0.0.1", 4001);
 
-    client.udp_bind();
+	client.udp_bind();
 
-    msg_A msg;
+	msg_A msg;
 
-    while (1)
-    {
-        client.udp_receive(&msg);
+	while (1)
+	{
+		client.udp_receive(&msg);
 
-        std::cout << "Receive_type: " << msg.type << std::endl;
-        std::cout << "Receive_x: " << msg.x << std::endl;
-    }
-    return 0;
+		std::cout << "Receive_type: " << msg.type << std::endl;
+		std::cout << "Receive_x: " << msg.x << std::endl;
+	}
+	return 0;
 }
---
+++ include/udp_lib_windows.hpp
@@ -0,0 +1,78 @@
+#include <winsock2.h>
+#include <stdio.h>
+#include <ws2tcpip.h>
+#include <stdio.h>
+#include <WinSock2.h>
+#include <Windows.h>
+#include <ws2tcpip.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+namespace udp
+{
+    class UDPLib
+    {
+    private:
+        SOCKET sock_;
+        struct sockaddr_in server_addr_;
+        struct sockaddr_in client_addr_;
+
+    public:
+        UDPLib(const std::string &address, const int port)
+        {
+
+            WSADATA wsaData;
+            WSAStartup(MAKEWORD(2, 0), &wsaData);
+            // Generate socket
+            // AF_INET: IPV4, SOCK_DGRAM: UDP, 0: protocol is automoatically chosen
+            sock_ = socket(AF_INET, SOCK_DGRAM, 0);
+            if (sock_ < 0)
+            {
+                std::cerr << "[Error] Socket initialize error." << std::endl;
+                exit(0);
+            }
+
+            // IP/port setting
+            server_addr_.sin_family = AF_INET;
+            server_addr_.sin_port = htons(port);
+            server_addr_.sin_addr.S_un.S_addr = INADDR_ANY;
+            InetPton(AF_INET, address.c_str(), &server_addr_.sin_addr.s_addr);
+        }
+
+        ~UDPLib()
+        {
+            closesocket(sock_);
+            WSACleanup();
+        }
+
+        void udp_bind() const
+        {
+            if (bind(sock_, (const struct sockaddr *)&server_addr_, sizeof(server_addr_)) < 0)
+            {
+                std::cerr << "[Error] UDP bind error." << std::endl;
+                exit(2);
+            }
+        }
+
+        template <typename MSG>
+        void udp_send(const MSG &msg) const
+        {
+            if (sendto(sock_, (char *)&msg, sizeof(msg), 0, (struct sockaddr *)&server_addr_, sizeof(server_addr_)) < 0)
+            {
+                std::cerr << "[Error] UDP send Error." << std::endl;
+                exit(1);
+            }
+        }
+
+        template <typename MSG>
+        void udp_receive(MSG *msg) const
+        {
+            socklen_t len = sizeof(client_addr_);
+            if (recvfrom(sock_, (char *)msg, sizeof(*msg), 0, (struct sockaddr *)&client_addr_, &len) < 0)
+            {
+                std::cerr << "[Error] UDP received data size is invalid." << std::endl;
+                exit(2);
+            }
+        }
+    };
+}
