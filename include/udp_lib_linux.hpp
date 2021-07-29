312b157942afb80cf66a056b94273798d190f9b0
kohonda <0905honda@gmail.com>
2021-07-30 00:14:03 +0900

    enable to set source port, only linux

--- include/udp_lib_linux.hpp
+++ include/udp_lib_linux.hpp
@@ -21,6 +21,12 @@ namespace udp
         struct sockaddr_in addr_;
 
     public:
+        /**
+    　　  * @brief Construct a new UDPLib object, normal usage
+    　　  * 
+    　　  * @param address: When publisher, Set destination IP address. When subscriber, Set source IP address.  
+    　　  * @param port: When publisher, Set destination port. When subscriber, Set source port.
+    　　  */
         UDPLib(const std::string &address, const int port)
         {
             // Generate socket
@@ -42,15 +48,55 @@ namespace udp
             ioctl(sock_, FIONBIO, &val);
         }
 
+        /**
+         * @brief Construct a new UDPLib object, can set source port
+         * 
+         * @param destination_address: When publisher, Set destination IP address. When subscriber, Set source IP address.  
+         * @param destination_port
+         * @param source_port 
+         */
+        UDPLib(const std::string &destination_address, const int destination_port, const int source_port)
+        {
+            // Generate socket
+            // AF_INET: IPV4, SOCK_DGRAM: UDP, 0: protocol is automoatically chosen
+            sock_ = socket(AF_INET, SOCK_DGRAM, 0);
+            if (sock_ < 0)
+            {
+                std::cerr << "[Error] Socket initialize error." << std::endl;
+                exit(0);
+            }
+
+            // IP/port setting
+            addr_.sin_family = AF_INET;
+            addr_.sin_addr.s_addr = inet_addr(destination_address.c_str());
+            addr_.sin_port = htons(destination_port);
+
+            // set source port
+            struct sockaddr_in source_addr;
+            source_addr.sin_family = AF_INET;
+            source_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+            source_addr.sin_port = htons(source_port);
+
+            // Non blocking setting
+            const auto val = 1; // 0: blocking, 1: non-blocking
+            ioctl(sock_, FIONBIO, &val);
+
+            // bind source port
+            if (bind(sock_, (const struct sockaddr *)&source_addr, sizeof(source_addr)) < 0)
+            {
+                std::cerr << "[Error] UDP bind error." << std::endl;
+
+                exit(2);
+            }
+        }
+
         void udp_bind() const
         {
             if (bind(sock_, (const struct sockaddr *)&addr_, sizeof(addr_)) < 0)
             {
                 std::cerr << "[Error] UDP bind error." << std::endl;
-                const int sleep_time = 1; // [msec]
-                std::this_thread::sleep_for(std::chrono::milliseconds(sleep_time));
 
-                // exit(2);
+                exit(2);
             }
         }
 
